9601617bf807b4c4a07f0b08cd7d37dbf58fc642
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-24 18:57:51 -0500

    feat(Main): add form component and material ui components

---
+++ src/containers/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '../../styledComponents/Container'
+import Form from '../../components/Form';
+
+class Index extends React.Component {
+  render() {
+    return (<Container>
+      <Form />
+      </Container>);
+  }
+}
+
+export default Index;
