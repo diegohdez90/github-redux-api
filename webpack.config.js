4a3d5c77b8491c4f3be3bd51a7706fa032b8cd7b
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-24 16:37:18 -0500

    chore(webpack): init config

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  entry: __dirname + "/src/index.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js)/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }, {
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            sourceMap: true,
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new webpack.ProvidePlugin({
+      'React': 'react'
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    open: true
+  }
+};
