93fd671386299b7bf4b00063e68a487c33caf43b
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-24 19:19:56 -0500

    feat(Main): add GetToken form in container

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Container from '../../styledComponents/Container'
 import Form from '../../components/Form';
-
+import GetToken from '../../components/GetToken';
 class Index extends React.Component {
   render() {
     return (<Container>
+      <GetToken />
       <Form />
       </Container>);
   }
