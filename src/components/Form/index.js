c7cdf1d8fb78955a0e29d3c81830978246f2810c
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-28 18:11:26 -0500

    refactor(Form): update form to get repos with auth token

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Input from '../../styledComponents/Input';
 import Label from '../../styledComponents/Label';
-import { updateGithubAccount, fetchUserRepos } from '../../actions';
+import { updateGithubAccount, fetchUserRepos, fetchAccount } from '../../actions';
 import Button from '../../styledComponents/Button';
 import {connect} from 'react-redux';
+
+const mapStateToProps = (state) => {
+  console.log('mapStatetoProps Form', state);
+  
+  return {
+    token: state.reducer.token
+  }
+}
 
 const mapDispatchToProps = dispatch => {  
   return {
     updateGithubAccount: githubAccount => {
       dispatch(updateGithubAccount(githubAccount))
     },
-    fetchUserRepos: (githubAccount) => {
-      dispatch(fetchUserRepos(githubAccount))
+    fetchUser: (githubAccount, token) => {
+      dispatch(fetchAccount(githubAccount, token))
+    },
+    fetchUserRepos: (githubAccount, token) => {
+      dispatch(fetchUserRepos(githubAccount, token))
     }
   }
 }
@@ -37,7 +48,9 @@ class FormComponent extends Component {
   onHandleSubmitGithubAccount(ev) {
     ev.preventDefault();
     this.props.updateGithubAccount(this.state.githubAccount);
-    this.props.fetchUserRepos(this.state.githubAccount);
+    console.log('state form', this.state);
+    this.props.fetchUser(this.state.githubAccount, this.props.token);
+    this.props.fetchUserRepos(this.state.githubAccount, this.props.token);
     this.setState({githubAccount: ''})
   }
 
@@ -55,9 +68,10 @@ class FormComponent extends Component {
   }
 }
 
-const Form = connect(null, mapDispatchToProps)(FormComponent)
+const Form = connect(mapStateToProps, mapDispatchToProps)(FormComponent)
 
 FormComponent.propTypes = {
+  token: PropTypes.string,
   onChangeGithubAccount: PropTypes.func,
   onHandleSubmitGithubAccount: PropTypes.func
 };
--- src/components/Form/saga.js
+++ src/components/Form/saga.js
@@ -1,12 +1,46 @@
 import { call, put, takeLatest }  from 'redux-saga/effects';
-import { FETCH_USER_REPOS, FETCH_USER_REPOS_SUCCESS, FETCH_USER_REPOS_FAILURE } from '../../utils/constants';
+import { FETCH_USER_REPOS,
+  FETCH_USER_REPOS_SUCCESS,
+  FETCH_USER_REPOS_FAILURE,
+  FETCH_ACCOUNT,
+  FETCH_ACCOUNT_SUCCESS,
+  FETCH_ACCOUNT_FAILURE
+ } from '../../utils/constants';
 import request from '../../utils/request';
 
-function* fetchUserRepos (action) {
+function* fetchAccount(action) {
+  console.log('fetch github account', action);
   
   try {
+    const {token} = action;
+    let headers = {};
+    if (token) {
+      headers = {
+        Authorization: `Basic ${token}`
+      }
+    }
+    const account = yield call(request, `https://api.github.com/users/${action.githubAccount}`, {
+      method: 'GET',
+      headers
+    });
+    yield put({ type: FETCH_ACCOUNT_SUCCESS, account });
+  } catch (error) {
+    yield put({type: FETCH_ACCOUNT_FAILURE, message: error.message })
+  }
+}
+
+function* fetchUserRepos (action) {
+  try {
+    const { token } = action;
+    let headers = {};
+    if (token) {
+      headers = {
+        Authorization: `Basic ${token}`
+      }
+    }
     const repos = yield call(request, `https://api.github.com/users/${action.githubAccount}/repos`, {
-      method: 'GET'
+      method: 'GET',
+      headers
     });
     yield put({ type: FETCH_USER_REPOS_SUCCESS, repos});
   } catch (error) {
@@ -15,6 +49,7 @@ function* fetchUserRepos (action) {
 }
 
 function* saga() {
+  yield takeLatest(FETCH_ACCOUNT, fetchAccount);
   yield takeLatest(FETCH_USER_REPOS, fetchUserRepos);
 }
 
