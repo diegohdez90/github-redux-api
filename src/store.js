02ef25c4a699f35a5e997528d8d52207bc97c77d
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-28 18:11:58 -0500

    chore(store): add saga to get auth token

--- src/store.js
+++ src/store.js
@@ -1,15 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import FormSaga from './components/Form/saga';
+import GetTokenSaga from './components/GetToken/saga';
 import reducer from './reducers';
 
 const sagaMiddleware = createSagaMiddleware();  //Create middleware for saga
+const sagaMiddlewareGetToken = createSagaMiddleware();
 
 const store = createStore(
   reducer,
-  applyMiddleware(sagaMiddleware)); //mount it into redux store
+  applyMiddleware(sagaMiddleware),
+  applyMiddleware(sagaMiddlewareGetToken)
+); //mount it into redux store
 
 
-sagaMiddleware.run(FormSaga); 
-
+sagaMiddleware.run(FormSaga);
+sagaMiddlewareGetToken.run(GetTokenSaga);
+ 
 export default store;
