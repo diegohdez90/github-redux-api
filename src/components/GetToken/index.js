0e657799666e35869eb51520b11f0721d82ede24
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-28 18:10:20 -0500

    feat(GetToken): init component to get token to auth github user

--- src/components/GetToken/index.js
+++ src/components/GetToken/index.js
@@ -1,20 +1,89 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Label from '../../styledComponents/Label';
 import Input from '../../styledComponents/Input';
 import Button from '../../styledComponents/Button';
+import { updateUsernameToken,
+  updatePasswordToken,
+  fetchGetToken } from '../../actions';
 
-class GetToken extends React.Component {
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateUsername: (username) => {
+      dispatch(updateUsernameToken(username))
+    },
+    updatePassword: (password) => {
+      dispatch(updatePasswordToken(password))
+    },
+    fetchUserToken: (username, password) => {
+      console.log('calling User Token', username, password);
+      
+      dispatch(fetchGetToken(username, password))
+    }
+  }
+}
+
+class GetTokenComponent extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onHandlerGetToken = this.onHandlerGetToken.bind(this);
+  }
+
+  onChangeUsername(ev) {
+    this.setState({
+      username: ev.target.value
+    });
+    this.props.updateUsername(ev.target.value);
+  }
+
+  onChangePassword(ev) {
+    this.setState({
+      password: ev.target.value
+    });
+    this.props.updatePassword(ev.target.value);
+  }
+
+  onHandlerGetToken(ev) {
+    ev.preventDefault();
+    this.props.fetchUserToken(this.state.username, this.state.password);
+  }
+
   render () {
-    return (<form>
+    return (<form onSubmit={this.onHandlerGetToken}>
       <Label>
-      Username: <Input />
+      Username: <Input
+        name="username"
+        onChange={this.onChangeUsername}
+        />
       </Label>
       <Label>
-        Password <Input type='password' />
+        Password <Input
+        type='password'
+        onChange={this.onChangePassword}
+        />
       </Label>
       <Button>Get Token</Button>
     </form>)
   }
 }
+
+const GetToken = connect(null, mapDispatchToProps)(GetTokenComponent);
+
+GetTokenComponent.propTypes = {
+  onChangeUsername: PropTypes.func,
+  onChangePassword: PropTypes.func,
+  onHandlerGetToken: PropTypes.func
+};
 
 export default GetToken;
---
+++ src/components/GetToken/saga.js
@@ -0,0 +1,30 @@
+import { Buffer } from 'buffer'
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { FETCH_GET_TOKEN, FETCH_GET_TOKEN_SUCCESS, FETCH_GET_TOKEN_FAILURE } from '../../utils/constants';
+import request from '../../utils/request';
+
+function* getUserToken(action){
+  console.log(action);
+  
+  const {username, password} = action;
+  try {
+    const buffer = new Buffer(`${username}:${password}`)
+    const encodeAuth = buffer.toString('base64')
+    const token = yield call(request, 'https://api.github.com/user', {
+      headers: {
+        Authorization: `Basic ${encodeAuth}`
+      }
+    })
+    console.log(token);
+    
+    yield put({type: FETCH_GET_TOKEN_SUCCESS, token: encodeAuth})
+  } catch(error) {
+    yield put({ type: FETCH_GET_TOKEN_FAILURE, message: error.message })
+  }
+}
+
+function* saga() {
+  yield takeLatest(FETCH_GET_TOKEN, getUserToken);
+}
+
+export default saga;
