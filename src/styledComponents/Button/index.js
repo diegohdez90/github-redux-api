75ab9ad2e322d4636d75b14657d6e83289bfce14
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-27 19:08:25 -0500

    style(Button): update width

--- src/styledComponents/Button/index.js
+++ src/styledComponents/Button/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   padding: 0.75em 0;
-  width: 30%;
+  width: 10%;
   background-color: #31d;
   border-radius: 0.5em;
   color: #ddd;
