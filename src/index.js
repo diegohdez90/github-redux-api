8af9523c555828ce8db753aa4b19835992e15056
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-27 19:11:23 -0500

    feat(app): add redux Provider and init babel polyfill for redux-saga

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import '@babel/polyfill'; //Start to use redux-saga
 import react from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './App';
+import store from './store';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(<Provider store={store} >
+  <App />
+  </Provider>, root);
