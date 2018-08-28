4bf2a28ac6d46659db4bb57088e546d8442e1141
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-27 19:14:46 -0500

    chore(Main): update Main container and import components

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -3,20 +3,16 @@ import PropTypes from 'prop-types';
 import Container from '../../styledComponents/Container'
 import Form from '../../components/Form';
 import GetToken from '../../components/GetToken';
+import Repos from '../../components/Repos';
 class Main extends React.Component {
+
   render() {
     return (<Container>
-      <GetToken />
+      <GetToken/>
       <Form />
-      </Container>);
+      <Repos />
+    </Container>);
   }
-}
-
-Main.propTypes ={
-  username: PropTypes.string,
-  password: PropTypes.string,
-  token: PropTypes.string,
-  repos: PropTypes.array
 }
 
 export default Main;
