2ca2b066803b849fb15b5aef21bd6a2d21938a03
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-29 15:13:35 -0500

    feat(Watches): delete saga from component and store
    install redux-thunk to use fetch in component

--- src/components/Watches/index.js
+++ src/components/Watches/index.js
@@ -3,19 +3,26 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Octicon, { Eye } from '@githubprimer/octicons-react';
 import SpanGithub from '../../styledComponents/GithubComponent';
-import { watchRepo } from '../../actions';
+import { watchRepo, watchRepoSuccess, watchRepoFailure } from '../../actions';
 
 const mapStateToProps = (state) => {
   return {
     token: state.reducer.token,
     githubAccount: state.reducer.githubAccount
-  }
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     watchRepo: (repo, account, token) => {
-      dispatch(watchRepo(repo, account, token))
+      fetch(`https://api.github.com/repos/${account}/${repo}/subscription`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Basic ${token}`
+        }
+      })
+      .then((message) => dispatch(watchRepoSuccess(message)))
+      .catch(err => dispatch(watchRepoFailure(err.message)))
     }
   };
 };
--- src/store.js
+++ src/store.js
@@ -1,24 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
+import thunk from 'redux-thunk';
 import FormSaga from './components/Form/saga';
 import GetTokenSaga from './components/GetToken/saga';
-import WatchRepoSaga from './components/Watches/saga';
 import reducer from './reducers';
 
 const sagaMiddlewareGetRepos = createSagaMiddleware();  //Create middleware for saga
 const sagaMiddlewareGetToken = createSagaMiddleware();
-const sagaMiddlewareWatchRepo = createSagaMiddleware();
+
 
 const store = createStore(
   reducer,
   applyMiddleware(sagaMiddlewareGetRepos),
   applyMiddleware(sagaMiddlewareGetToken),
-  applyMiddleware(sagaMiddlewareWatchRepo)
+  applyMiddleware(thunk)
 ); //mount it into redux store
-
 
 sagaMiddlewareGetRepos.run(FormSaga);
 sagaMiddlewareGetToken.run(GetTokenSaga);
-sagaMiddlewareWatchRepo.run(WatchRepoSaga);
 
 export default store;
