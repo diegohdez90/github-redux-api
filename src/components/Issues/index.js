53b19d6329649531bdf9bfce4385085ab0383cc0
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-08-29 19:11:38 -0500

    feat(Issues): update RepoDetails to class component to use state toggle function to display table issues

---
+++ src/components/Issues/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from '../../styles/Title';
+
+const Issues = () => (
+  <div>
+    <Title>Issues</Title>
+  </div>
+)
+
+export default Issues;
--- src/components/RepoDetails/index.js
+++ src/components/RepoDetails/index.js
@@ -3,13 +3,36 @@ import PropTypes from 'prop-types';
 import Forks from '../Forks';
 import Stars from '../Stars';
 import Watches from '../Watches';
+import Issues from '../Issues';
+import Title from '../../styles/Title';
 
-const RepoDetails = ({watches, stars, forks, name}) => (
-  <div>
-    <Watches watches={watches} repo={name}/>
-    <Stars stars={stars} repo={name} />
-    <Forks forks={forks} />
-  </div>);
+class RepoDetails extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {
+      toggleIssues: false
+    };
+  }
+
+  toggleIssuesTable () {
+    this.setState({
+      toggleIssues: !this.state.toggleIssues
+    });
+  }
+  render () {
+    const { watches, stars, forks, name } = this.props;
+    return (
+      <div>
+        <Watches watches={watches} repo={name}/>
+        <Stars stars={stars} repo={name} />
+        <Forks forks={forks} />
+        <div onClick={() => this.toggleIssuesTable()}>
+          {(this.state.toggleIssues) ? <Issues /> : <Title>Looking for Issues</Title> }
+        </div>
+      </div>);
+  }
+}
 
 RepoDetails.propTypes ={
   name: PropTypes.string,
