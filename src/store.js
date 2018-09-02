8d9d27747811dd02b4eda88cea933fc150cb4eaa
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-01 19:42:36 -0500

    refactor(app): update combineReducers arguments
    add middleware to run Main container saga

--- src/store.js
+++ src/store.js
@@ -1,22 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import thunk from 'redux-thunk';
-import FormSaga from './components/Form/saga';
-import GetTokenSaga from './components/GetToken/saga';
+import MainSaga from './containers/Main/saga';
 import reducer from './reducers';
 
-const sagaMiddlewareGetRepos = createSagaMiddleware();  //Create middleware for saga
-const sagaMiddlewareGetToken = createSagaMiddleware();
+const sagaMiddlewareMain = createSagaMiddleware();  //Create middleware for saga
 
 
 const store = createStore(
   reducer,
-  applyMiddleware(sagaMiddlewareGetRepos),
-  applyMiddleware(sagaMiddlewareGetToken),
+  applyMiddleware(sagaMiddlewareMain),
   applyMiddleware(thunk)
 ); //mount it into redux store
 
-sagaMiddlewareGetRepos.run(FormSaga);
-sagaMiddlewareGetToken.run(GetTokenSaga);
+sagaMiddlewareMain.run(MainSaga);
 
 export default store;
