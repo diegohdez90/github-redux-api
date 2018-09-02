1e464c9e1043c7b68c097d970a799338e2d6160e
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-01 19:41:24 -0500

    chore(Main): move Form and GetToken sagas to Main container

---
+++ src/containers/Main/saga.js
@@ -0,0 +1,69 @@
+import { Buffer } from 'buffer'
+import { call, put, takeLatest } from 'redux-saga/effects';
+import request from '../../utils/request';
+import { FETCH_GET_TOKEN, FETCH_GET_TOKEN_SUCCESS, FETCH_GET_TOKEN_FAILURE, FETCH_ACCOUNT, FETCH_ACCOUNT_SUCCESS, FETCH_ACCOUNT_FAILURE, FETCH_USER_REPOS, FETCH_USER_REPOS_SUCCESS, FETCH_USER_REPOS_FAILURE } from '../../utils/constants';
+
+function* getUserToken(action){  
+  const {username, password} = action;
+  try {
+    const buffer = new Buffer(`${username}:${password}`)
+    const encodeAuth = buffer.toString('base64')
+    const auth = yield call(request, 'https://api.github.com/user', {
+      headers: {
+        Authorization: `Basic ${encodeAuth}`
+      }
+    })
+    if ('name' in auth) yield put({ type: FETCH_GET_TOKEN_SUCCESS, token: encodeAuth });
+    else yield put({ type: FETCH_GET_TOKEN_FAILURE, message: auth });
+  } catch(error) {
+    yield put({ type: FETCH_GET_TOKEN_FAILURE, message: error.message });
+  }
+}
+
+function* fetchAccount(action) {
+  try {
+    const { token } = action;
+    let headers = {};
+    if (token) {
+      headers = {
+        Authorization: `Basic ${token}`
+      }
+    }
+    const account = yield call(request, `https://api.github.com/users/${action.githubAccount}`, {
+      method: 'GET',
+      headers
+    });
+    if ('name' in account) yield put({ type: FETCH_ACCOUNT_SUCCESS, account });
+    else yield put({ type: FETCH_ACCOUNT_FAILURE, message: account });
+  } catch (error) {
+    yield put({ type: FETCH_ACCOUNT_FAILURE, message: error.message })
+  }
+}
+
+function* fetchUserRepos(action) {
+  try {
+    const { token } = action;
+    let headers = {};
+    if (token) {
+      headers = {
+        Authorization: `Basic ${token}`
+      }
+    }
+    const repos = yield call(request, `https://api.github.com/users/${action.githubAccount}/repos`, {
+      method: 'GET',
+      headers
+    });
+    if (Array.isArray(repos)) yield put({ type: FETCH_USER_REPOS_SUCCESS, repos });
+    else yield put({ type: FETCH_USER_REPOS_FAILURE, message: repos })
+  } catch (error) {
+    yield put({ type: FETCH_USER_REPOS_FAILURE, message: error.message })
+  }
+}
+
+function* saga() {
+  yield takeLatest(FETCH_GET_TOKEN, getUserToken);
+  yield takeLatest(FETCH_ACCOUNT, fetchAccount);
+  yield takeLatest(FETCH_USER_REPOS, fetchUserRepos);
+}
+
+export default saga;
