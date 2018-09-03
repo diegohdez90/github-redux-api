f3098980f8e22e0b0118262f1c9d5aff9e3a0726
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-03 12:20:01 -0500

    chore(app): update files for eslint, gitattributes and editorconfig

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,9 @@
 import 'whatwg-fetch';
-
 function parseJSON(response) {
   return response.json();
 }
 
 export default function request (url, opts) {
   return fetch(url, opts)
-  .then(parseJSON);
+    .then(parseJSON);
 }
